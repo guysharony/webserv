23a6fbec5b3487a014518e0e1452b3b17c2a1f6a
yesmine-code <yesminerekik5@gmail.com>
2022-03-31 11:30:47 +0200

    add timeout getter

--- src/response/response.cpp
+++ src/response/response.cpp
@@ -87,6 +87,7 @@ std::string   response::createBody(){
     Config::location_type loc = _req.selectLocation(_server);
     if (_codeDeRetour == STATUS_NOT_FOUND)
         return (readHtmlFile(_server.error_page[404]));
+        return (readHtmlFile("www/errors/404.html"));// normalement c _server.error_page[404] mais error_page c toujours vide
     if (_codeDeRetour == STATUS_OK){
         if (_server.root.size() > 0){
             if (_req.getPath().compare(loc->location) == 0){
@@ -176,6 +177,5 @@ std::string       response::getUrl(std::string dirent, std::string dirName) {
     ss << "\t\t<p><a href=\"http://" + _req.getHost() + ":" <<_req.getPort() << dirName + "/" + dirent + "\">" + dirent + "</a></p>\n";
     return ss.str();
 }
-
 
 
