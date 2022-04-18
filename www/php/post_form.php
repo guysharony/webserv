0b7f50c5bf0b7073cfd9b13b7b312f64d6b6188a
Guy Sharony <gsharony@student.42.fr>
2022-04-18 12:06:55 +0200

    Preparing post form request

--- www/php/post_form.php
+++ www/php/post_form.php
@@ -4,17 +4,18 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Form example</title>
+	<title>POST Request form</title>
 </head>
 <body>
-	<?php if (isset($_GET['first_name']) && isset($_GET['last_name'])) { ?>
+	<?php if (isset($_POST['first_name']) && isset($_POST['last_name'])) { ?>
 		<div>
-			<p>Received GET request</p>
-			<p>First name: <?php echo $_GET['first_name']; ?></p>
-			<p>Last name: <?php echo $_GET['last_name']; ?></p>
+			<p>Received POST request</p>
+			<p>First name: <?php echo $_POST['first_name']; ?></p>
+			<p>Last name: <?php echo $_POST['last_name']; ?></p>
 		</div>
 	<?php } ?>
-	<form action="/php/form.php" method="GET">
+	<h1>POST Request</h1>
+	<form action="/php/post_form.php" method="POST">
 		<div>
 			<label for="first_name">First name</label>
 			<input type="text" name="first_name" id="first_name" value="">
