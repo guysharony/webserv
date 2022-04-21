20530eeac2fc14bb7800fe8d9ca51e03d111a931
tmorris <tmorris@student.42.fr>
2022-04-21 12:19:46 +0200

    Fix CGI formatting

--- src/cgi/CGI.cpp
+++ src/cgi/CGI.cpp
@@ -1,26 +1,21 @@
 #include "CGI.hpp"
 
 CGI::CGI(Request *request)
-:
-	_request(request)
-{ }
+	: _request(request)
+{
+}
 
-/*
-CGI::CGI(std::string const &cgi_path)
-: _cgi_path(cgi_path)
-{ }
-*/
-
-CGI::CGI(CGI const & src)
-:
-	_request(src._request)
-{ }
+CGI::CGI(CGI const &src)
+	: _request(src._request)
+{
+}
 
 CGI::~CGI(void)
 {
 }
 
-CGI &CGI::operator=(CGI const & rhs) {
+CGI &CGI::operator=(CGI const &rhs)
+{
 	if (this != &rhs)
 	{
 		this->_request = rhs._request;
@@ -30,15 +25,15 @@ CGI &CGI::operator=(CGI const & rhs) {
 
 int CGI::_init_env(std::string const &filename)
 {
-	Config::configuration_type        	server = this->_request->selectServer();
-	Config::location_type             	location = this->_request->selectLocation(server);
-	std::map<std::string, std::string>	headers = this->_request->getHeader();
+	Config::configuration_type server = this->_request->selectServer();
+	Config::location_type location = this->_request->selectLocation(server);
+	std::map<std::string, std::string> headers = this->_request->getHeader();
 
 	// Prepare environment for execve
 	clearenv();
-	setenv("SERVER_SOFTWARE", SERVER_NAME, true);						// *** This value should be confirmed
+	setenv("SERVER_SOFTWARE", SERVER_NAME, true); // *** This value should be confirmed
 	setenv("SERVER_NAME", server->server_name.c_str(), true);
-	setenv("GATEWAY_INTERFACE", "CGI/1.1", true);						// *** This value should be confirmed
+	setenv("GATEWAY_INTERFACE", "CGI/1.1", true); // *** This value should be confirmed
 	setenv("SERVER_PROTOCOL", "HTTP/1.1", true);
 	setenv("SERVER_PORT", this->_request->getPort().c_str(), true);
 	setenv("PATH_INFO", filename.c_str(), true);
@@ -62,7 +57,6 @@ int CGI::_init_env(std::string const &filename)
 		setenv(toUppercase(key).c_str(), it->second.c_str(), true);
 	}
 
-
 	std::string content_length = toString(this->_request->sizeTemporary("request"), false);
 	if (this->_request->getMethod() == "POST")
 		setenv("CONTENT_LENGTH", content_length.c_str(), true);
@@ -72,14 +66,14 @@ int CGI::_init_env(std::string const &filename)
 
 int CGI::_redirect_io(int fd[2])
 {
-	int		tmp_fd;
+	int tmp_fd;
 
 	if (this->_request->getMethod() == "POST")
 	{
 		tmp_fd = this->_request->fdTemporary("request");
 		if (tmp_fd < 0)
 			return (-1);
-		
+
 		lseek(tmp_fd, 0, SEEK_SET);
 		if (dup2(tmp_fd, STDIN_FILENO) < 0)
 			return (-1);
@@ -93,13 +87,14 @@ int CGI::_redirect_io(int fd[2])
 	return (0);
 }
 
-int	CGI::launch_cgi(std::string const & filename) {
-	int		fd[2];
-	pid_t	pid;
-	char	*executable;
-	char	*argument;
+int CGI::launch_cgi(std::string const &filename)
+{
+	int fd[2];
+	pid_t pid;
+	char *executable;
+	char *argument;
 
-	Config::configuration_type        	server = this->_request->selectServer();
+	Config::configuration_type server = this->_request->selectServer();
 
 	if (pipe2(fd, O_NONBLOCK))
 		return (-1);
@@ -115,7 +110,7 @@ int	CGI::launch_cgi(std::string const & filename) {
 
 		executable = &(server->cgi_path[0]);
 		argument = const_cast<char *>(&filename[0]);
-		char * const argv[3] = {executable, argument, NULL};
+		char *const argv[3] = {executable, argument, NULL};
 
 		execve(executable, argv, environ);
 		Message::error("Internal Error: could not execute CGI"); // *** Consider printing Status to stdout ?
