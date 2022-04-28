c99231779359ffc947ea622c98ada9699620ebf6
tmorris <tmorris@student.42.fr>
2022-04-28 12:34:11 +0200

    Update upload.php

--- www/php/w3_upload.php
+++ www/php/w3_upload.php
@@ -7,7 +7,7 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  echo "Sorry, file already exists. ";
   $uploadOk = 0;
 }
 
@@ -16,6 +16,7 @@ if ($uploadOk == 0) {
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+	http_response_code(201);
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
